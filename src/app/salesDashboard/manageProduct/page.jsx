9a48f76fc3d3c552c42de687b417c7d556d65d2d
JKalith <Jamesarias2383@gmail.com>
2024-10-29 20:07:03 -0600

    Actualización componente ventas
    Se ordeno el componente y la ventana emergente

--- src/app/salesDashboard/manageProduct/page.jsx
+++ src/app/salesDashboard/manageProduct/page.jsx
@@ -1,17 +1,43 @@
 "use client";
 import stylesDashboard from "../../styles/dashboard.module.css";
+import React, { useState } from "react";
+import Modal from "../../ui/popUpWindow/window";
+import icon from "../../ui/styles/icons.module.css";
 
-import TableProducts from '../../salesDashboard/manageProduct/tableProduct'
+import CreateProduct from "../../salesDashboard/manageProduct/createProduct";
+
+import TableProducts from "../../salesDashboard/manageProduct/tableProduct";
 export default function manageProduct() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className={stylesDashboard.container}>
-      
       <span>
         <p className={stylesDashboard.titlePage}>Gestion de producto</p>
 
         <div className={stylesDashboard.table}>
-        <button>Registrar producto</button>
-       <TableProducts></TableProducts>
+          <button onClick={openModal}>
+            {" "}
+            <div className={`${icon.containerIcon} ${icon.buyIcon}`}></div>
+            Ventana emergente
+          </button>
+
+          <Modal
+            isOpen={isModalOpen}
+            onClose={closeModal}
+            symbol={icon.closeIcon}
+          >
+            <CreateProduct></CreateProduct>
+          </Modal>
+          <TableProducts></TableProducts>
         </div>
       </span>
     </div>
